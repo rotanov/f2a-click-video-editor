8be42baa658258ef057d14d79110fe9a1bd5daa6
Denis Rotanov <mapholameth@gmail.com>
2017-02-21 00:02:35 +1000

    fix css; add videoplayer able to play video from local filesystem

--- react-app/f2a-click-video-editor/src/app.js
+++ react-app/f2a-click-video-editor/src/app.js
@@ -2,30 +2,49 @@ import React, { Component } from 'react';
 import './app.css';
 
 class App extends Component {
+  URL = window.URL || window.webkitURL;
+  displayMessage(message, isError) {
+    console.log(message);
+  }
+  playSelectedFile(event) {
+    console.log(event);
+    let file = event.target.files[0];
+    let type = file.type;
+    let videoNode = document.querySelector('video');
+    let canPlay = videoNode.canPlayType(type);
+    if (canPlay === '') {
+      canPlay = 'no';
+    }
+    let message = 'Can play type "' + type + '": ' + canPlay;
+    let isError = canPlay === 'no';
+    this.displayMessage(message, isError);
+    if (isError) {
+      return;
+    }
+    let fileURL = URL.createObjectURL(file);
+    videoNode.src = fileURL;
+  }
   render() {
     return (
-      <div className="app-root-wrapper">
       <div className="app-root">
         <div className="app-header">
           PRESS F2-A-CLICK TO WIN
+          <input type="file" accept="video/*" onChange={(e) => this.playSelectedFile(e)}/>
         </div>
-        <div className="video-panel-root">
-          VIDEO
-        </div>
-        <div className="cutlist-panel-root">
-          CUTLIST
+        <div className="app-root-middle-row">
+          <div className="video-panel-root">
+            <video controls autoPlay className="video-player"></video>
+          </div>
+          <div className="cutlist-panel-root">
+            CUTLIST
+          </div>
         </div>
         <div className="cut-panel-root">
           CUT
         </div>
-      </div>
       </div>
     );
   }
 }
 
 export default App;
-
-
-        // <input type="file" accept="video/*"/>
-        // <video controls autoPlay></video>
